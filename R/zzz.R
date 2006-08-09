7bebb238dc55dc83264c75091a28daefa97b1f91
Korbinian Strimmer <korbinian.strimmer@lmu.de>
2006-08-09 00:00:00 +0000

    version 2.12.0

---
+++ R/zzz.R
@@ -0,0 +1,14 @@
+
+.First.lib <- function(lib, pkg) {
+    
+  cat("\n\n\n")
+  cat("-----------------------------------------------------------------\n")
+  cat("With the present version, GeneTS has become a meta-package that\n")
+  cat("exists mainly for reasons of backward compatibility.\n")
+  cat("\n")
+  cat("Its only purpose is to load the GeneCycle and GeneNet packages.\n")
+  cat("You may load either of these packages directly.\n")
+  cat("-----------------------------------------------------------------\n")
+  cat("\n\n\n")
+}
+
